6db48599d43ea8282d4a750e65df078db8d28a75
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-22 19:24:27 +0200

    feature: fix inifity case

--- src/features/calculator/controller/codecs.js
+++ src/features/calculator/controller/codecs.js
@@ -2,14 +2,14 @@ const { OPERATION } = require('../config');
 const t = require('io-ts');
 const { NumberFromString } = require('io-ts-types');
 
-const OperationsBrand =  t.keyof(OPERATION);
+const OperationsBrand = t.keyof(OPERATION);
 
 const CalculatorRequestCodec = t.strict({
-    a: t.union([NumberFromString, t.number]),
-    b: t.union([NumberFromString, t.number]),
-    op: OperationsBrand
+  a: t.union([NumberFromString, t.number]),
+  b: t.union([NumberFromString, t.number]),
+  op: OperationsBrand,
 });
 
 module.exports = {
-    CalculatorRequestCodec,
-}
+  CalculatorRequestCodec,
+};
--- src/features/calculator/controller/codecs.test.js
+++ src/features/calculator/controller/codecs.test.js
@@ -1,137 +1,137 @@
-const {CalculatorRequestCodec} = require("./codecs");
+const { CalculatorRequestCodec } = require('./codecs');
 
-describe('Features - Calculator - Controller - Codec - Unit Test',  () => {
-    describe('CalculatorRequestCodec',  () => {
-        describe('add and +',  () => {
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "add"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "+"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-        });
-        describe('minus and -', () => {
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "minus"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "-"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-        });
-        describe('multiple and *', () => {
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "multiple"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "*"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-        });
-        describe('divide and /', () => {
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "divide"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-            test('valid calculator request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "/"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeTruthy();
-            });
-        });
-
-        describe('valid number request', () => {
-           test('should allow numbers', () => {
-               const request = {
-                   body: {   "a": 2,   "b": 2,   "op": "add" }
-               }
-               const act = CalculatorRequestCodec.decode(request.body);
-               expect(act.right).toBeTruthy();
-           });
-        });
-
-        describe('unknown or codec error', () => {
-            test('invalid request', () => {
-                const request = {
-                    body: {
-                        a: "12",
-                        b: "12",
-                        op: "unknown op"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeFalsy();
-            });
-            test('invalid request', () => {
-                const request = {
-                    body: {
-                        a: "two",
-                        b: "two",
-                        op: "+"
-                    }
-                }
-                const act = CalculatorRequestCodec.decode(request.body);
-                expect(act.right).toBeFalsy();
-            });
-        });
+describe('Features - Calculator - Controller - Codec - Unit Test', () => {
+  describe('CalculatorRequestCodec', () => {
+    describe('add and +', () => {
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: 'add',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: '+',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
     });
+    describe('minus and -', () => {
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: 'minus',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: '-',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+    });
+    describe('multiple and *', () => {
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: 'multiple',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: '*',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+    });
+    describe('divide and /', () => {
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: 'divide',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+      test('valid calculator request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: '/',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+    });
+
+    describe('valid number request', () => {
+      test('should allow numbers', () => {
+        const request = {
+          body: { a: 2, b: 2, op: 'add' },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeTruthy();
+      });
+    });
+
+    describe('unknown or codec error', () => {
+      test('invalid request', () => {
+        const request = {
+          body: {
+            a: '12',
+            b: '12',
+            op: 'unknown op',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeFalsy();
+      });
+      test('invalid request', () => {
+        const request = {
+          body: {
+            a: 'two',
+            b: 'two',
+            op: '+',
+          },
+        };
+        const act = CalculatorRequestCodec.decode(request.body);
+        expect(act.right).toBeFalsy();
+      });
+    });
+  });
 });
