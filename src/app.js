470ffe7ff3fa1bd58d1932f05f3b734377d7f0b5
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-22 03:12:45 +0200

    setup: adding default route

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const useragent = require('express-useragent');
+const app = express();
+
+app.use(useragent.express());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Muzikayise Flynn Buthelezi (zuluCoda) node rest api');
+});
+
+
+module.exports = app;
