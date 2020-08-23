bee8477bfc0f245de58076911f34c09d94215e47
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-23 18:11:46 +0200

    feature: adding imageMl route

---
+++ src/features/image-ml/config.js
@@ -0,0 +1,8 @@
+const IMAGE_SIZE = 5;
+const BUCKET = 'image-ml';
+const IMAGE_TYPE = 'image/jpeg';
+module.exports = {
+    IMAGE_SIZE,
+    BUCKET,
+    IMAGE_TYPE
+}
---
+++ src/features/image-ml/route/index.js
@@ -0,0 +1,5 @@
+const uploadToStore = require('../middleware/upload-to-store');
+const validate = require('../middleware/validate');
+const { imageMl } = require('../controller');
+
+module.exports = (app) => app.route('/Image').post(validate,uploadToStore, imageMl);
