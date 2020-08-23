c009d111a366c634255f7b796b102d351a8b4bf0
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-23 18:09:48 +0200

    feature: adding upload to S3 middleware

---
+++ src/features/image-ml/middleware/upload-to-store/index.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+const { BUCKET } = require('../../config');
+
+const uploadToS3 = (req, res, next) => {
+    const config = new AWS.Config({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    });
+
+    const s3 = new AWS.S3();
+
+    const image = {
+      Bucket: BUCKET,
+      Key: uuidv4(),
+      Body: req.body
+    };
+
+    s3.upload(image, (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error upload file to S3');
+        }
+        req.image = {
+            ...data
+        };
+        next();
+    });
+};
+
+const uploadToStore = [
+    uploadToS3,
+]
+
+module.exports = uploadToStore;
